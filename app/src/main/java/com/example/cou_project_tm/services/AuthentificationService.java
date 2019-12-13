ed789d9400f30aa57ba06c24c0167d95ee26b33d
Gormji <pierre.cardinal20@gmail.com>
2019-12-13 21:25:11 +0100

    RequestInterceptor implementation

--- app/src/main/java/com/example/cou_project_tm/services/AuthentificationService.java
+++ app/src/main/java/com/example/cou_project_tm/services/AuthentificationService.java
@@ -1,13 +1,28 @@
 package com.example.cou_project_tm.services;
 
 
+import com.example.cou_project_tm.RequestInterceptor;
+import com.example.cou_project_tm.config.Configuration;
 import com.example.cou_project_tm.models.AuthenticateModel;
 import com.example.cou_project_tm.models.User;
 
+import okhttp3.OkHttpClient;
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.HTTP;
 
 public class AuthentificationService {
     private static final AuthentificationService ourInstance = new AuthentificationService();
+
+    public static Call<User> getCurrentUser() {
+        return currentUser;
+    }
+
+    public static void setCurrentUser(Call<User> currentUser) {
+        AuthentificationService.currentUser = currentUser;
+    }
+
     private static Call<User> currentUser;
     private static UserRepoService repository;
     private static AuthenticateModel auth;
